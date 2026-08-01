b80189e7f5b04ae069c54e9f17b02f72f402388c
Jairo Matías <jairo_1829@hotmail.com>
2026-07-31 20:38:36 -0400

    chore: update default administrator credentials

--- config/rentadrive.php
+++ config/rentadrive.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'seed' => [
-        'admin_email' => env('RENTADRIVE_ADMIN_EMAIL', 'admin@rentadrive.test'),
-        'admin_password' => env('RENTADRIVE_ADMIN_PASSWORD', 'password'),
+        'admin_email' => env('RENTADRIVE_ADMIN_EMAIL', 'admin@rentadrive.com.do'),
+        'admin_password' => env('RENTADRIVE_ADMIN_PASSWORD', 'RentaDrive123..'),
     ],
 ];
