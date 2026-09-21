bfa2e928eef453dba6049c9057cfb6a2ea158fa2
Jairo Matías <jairo_1829@hotmail.com>
2026-09-21 01:23:16 -0400

    feat: include local platform administrator seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,13 +9,11 @@ class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call([
             RolePermissionSeeder::class,
+            PlatformAdminSeeder::class,
             AdminUserSeeder::class,
             OperationalReferenceSeeder::class,
         ]);
