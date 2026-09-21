09d2f910fa408de8c2f240cd489906d0f15c31e8
Jairo Matías <jairo_1829@hotmail.com>
2026-09-21 01:23:13 -0400

    config: add local platform admin credentials

--- config/rentadrive.php
+++ config/rentadrive.php
@@ -6,5 +6,7 @@ return [
     'seed' => [
         'admin_email' => env('RENTADRIVE_ADMIN_EMAIL', 'admin@rentadrive.com.do'),
         'admin_password' => env('RENTADRIVE_ADMIN_PASSWORD', 'RentaDrive123..'),
+        'platform_admin_email' => env('RENTADRIVE_PLATFORM_ADMIN_EMAIL', 'platform@rentadrive.test'),
+        'platform_admin_password' => env('RENTADRIVE_PLATFORM_ADMIN_PASSWORD', 'RentaDrivePlatform123!'),
     ],
 ];
